daf5ca229e1952f2567ceb81c76c57b91b3544d6
20230557 <j.j.h.bons@tue.nl>
2025-06-25 11:35:33 +0200

    initial function local repo done

--- calculate_mean.py
+++ calculate_mean.py
@@ -4,6 +4,9 @@ def calculate_mean(date):
 """ This function computes the mean of an array
 Input: One dimensional array
 Output: the mean of array
+Usage:
+    data=np.array([0,10,20,30])
+    mean=calculate_mean(data)
 """
 	mean=np.mean(data)
 	return mean
